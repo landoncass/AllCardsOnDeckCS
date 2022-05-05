7fd1ab5c013719619895c31b253cd114fb6cc3db
Landon Cassman <Landon.Cassman@gmail.com>
2022-05-05 15:24:44 -0400

    A program that creates a deck of cards, shuffles it, then deals two hands of two cards.

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ namespace AllCardsOnDeckCS
     {
         static void Main(string[] args)
         {
+            //Here is how you create a deck of cards
             var deck = new List<string>() { };
-            for (var suitValue = 0; suitValue < 4; suitValue++)
+            for (var suitValue = 4 - 1; suitValue >= 0; suitValue--)
             {
                 string suit = "Suit Name";
                 if (suitValue == 0)
@@ -82,12 +83,32 @@ namespace AllCardsOnDeckCS
                         deck.Add($"King of {suit}");
                     }
                 }
-
             }
-            for (var i = 0; i < 52; i++)
+            //Here is how we shuffle the deck
+            var numberOfCards = deck.Count;
+            for (var rightIndex = numberOfCards - 1; rightIndex > 0; rightIndex--)
             {
-                Console.WriteLine(deck[i]);
+                var randomNumberGenerator = new Random();
+                var leftIndex = randomNumberGenerator.Next(rightIndex);
+                var leftCard = deck[leftIndex];
+                var rightCard = deck[rightIndex];
+                deck[rightIndex] = leftCard;
+                deck[leftIndex] = rightCard;
             }
+            //Drawing cards from the deck and displaying the top two cards
+            var card1 = deck[0];
+            var card2 = deck[1];
+            var card3 = deck[2];
+            var card4 = deck[3];
+
+            Console.WriteLine(card1);
+            Console.WriteLine(card2);
+
+            //Creating two hands of two different cards and displaying what each hand is
+            var playerHand = new List<string>() { card1, card2 };
+            Console.WriteLine($"In my hand I have a {playerHand[0]} and a {playerHand[1]}");
+            var playerHand2 = new List<string>() { card3, card4 };
+            Console.WriteLine($"My second hand has a {playerHand2[0]} and a {playerHand2[1]}");
         }
     }
 }
